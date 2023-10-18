b28ec7d5017e47e55edf60507e2fde939b0a24f0
farhanshahriyar <farhan.abir9999@gmail.com>
2023-10-18 21:00:04 +0600

    added delete functionalities in backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -43,6 +43,14 @@ async function run() {
       const cursor = coffeeCollection.find({});
       const result = await cursor.toArray();
       res.send(result); // checking the data is available or not
+    });
+
+    // all delete requests here
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.json(result);
     });
 
 
